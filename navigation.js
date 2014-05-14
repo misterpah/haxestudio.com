479b55e503442e09175bca9799eeea91f760b7db
misterpah <misterpah@gmail.com>
2014-05-15 00:30:46 +0800

    preparing for v0.2

--- navigation.js
+++ navigation.js
@@ -18,17 +18,17 @@ if (document.title == "Haxe Studio")
 
 function url()
 	{
-	return "http://www.haxestudio.com/";
+	return "";
 	}
-	
-$("#menu-content").append(generateLink(url()+"./getting-started.html","Getting Started","Haxe Studio : Getting Started"));
-$("#menu-content").append(generateLink(url()+"./dist/index.html","Download","Haxe Studio : Download"));
-$("#menu-content").append(generateLink(url()+"./using-haxe-studio.html","Using Haxe Studio (outdated)","Haxe Studio : Using Haxe Studio"));
-$("#menu-content").append(generateLink(url()+"./contributors/index.html","Contributors","Haxe Studio : Contributors"));
 
-$("#menu-right").append('<li><a style="padding-top:5px;padding-bottom:5px;" href="https://plus.google.com/113245482496557815887" rel="publisher"><img height=40px src="'+url()+'./images/Google-plus-icon.png"/></a></li>');
+$("#menu-content").append(generateLink(url()+"./dist.html","Download","Haxe Studio : Download"));	
+$("#menu-content").append(generateLink(url()+"./getting-started.html","Getting Started","Haxe Studio : Getting Started"));
+//$("#menu-content").append(generateLink(url()+"./using-haxe-studio.html","Using Haxe Studio (outdated)","Haxe Studio : Using Haxe Studio"));
+$("#menu-content").append(generateLink(url()+"./contributors.html","Contributors","Haxe Studio : Contributors"));
+
+$("#menu-right").append('<li></li>');
 //$("body").append('<div id="gittip_wrap"><script data-gittip-username="misterpah" src="//gttp.co/v1.js"></script></div><style>#gittip_wrap{position:absolute;right:10px;top:60px;</style>');
 
-$(".navbar-brand").attr("href","http://www.haxestudio.com");
+$(".navbar-brand").attr("href","index.html");
 
 loadJS("http://www.haxestudio.com/js/google-analytics.js");
